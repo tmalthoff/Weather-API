8aac035d8b811f523a9c321f0b18a437d41fd92f
Thomas Althoff <matthewalthoff1@gmail.com>
2020-09-01 17:57:31 -0500

    Basic work

--- Assets/script.js
+++ Assets/script.js
@@ -21,12 +21,16 @@ $(document).ready(function () {
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b462a7368deee03b077f78e9fa37bc16&units=imperial"
         }).then(function (response) {
             console.log(response)
+            var temp = Math.round(response.main.temp)
+
             var card = $("<div>").addClass("card");
             var cardBody = $("<div>").addClass("card-body");
 
             var cityName = $("<h1>").addClass("card-title").text(response.name);
-
-            $("#today").append(card.append(cardBody.append(cityName)));
+            var tempEl = $("<h5>").addClass("card-text").text("Current Tempature is: " + temp);
+            var latAndLon = $("<h5>").addClass("card-text").text("Latitude: " + response.coord.lat + " and Longitude: " + response.coord.lon)
+            var humidity = $("<h5>").addClass("card-text").text("Humidity: " + response.main.humidity)
+            $("#today").append(card.append(cardBody.append(cityName, tempEl, latAndLon, humidity)));
 
             getForecast(response.coord.lat, response.coord.lon)
         })
@@ -36,17 +40,20 @@ $(document).ready(function () {
         $.ajax({
             method: "GET",
             dataType: "json",
-            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=b462a7368deee03b077f78e9fa37bc16`,
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=b462a7368deee03b077f78e9fa37bc16&units=imperial`,
             success: function (response) {
                 console.log(response)
 
                 for(var i = 1; i < 6; i++) {
-                    console.log(response.daily[i])
+                    var day = response.daily[i]
+
+
                     var card = $("<div>").addClass("card");
                     var cardBody = $("<div>").addClass("card-body");
-                    var date = $("<h1>").addClass("card-title").text(moment.unix(response.daily[i].dt).format("dddd, MMMM Do"));
+                    var date = $("<h1>").addClass("card-title").text(moment.unix(day.dt).format("dddd, MMMM Do"));
+                    var tempEl = $("<p>").addClass("card-title").text("Current Temperature is: " + day.temp.day )
 
-                    $("#forecast").append(card.append(cardBody.append(date)));
+                    $("#forecast").append(card.append(cardBody.append(date, tempEl)));
                 }
             }
         })
